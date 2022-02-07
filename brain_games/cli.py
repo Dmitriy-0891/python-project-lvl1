788e74aefe4bd36e4fc52542492bd8ce01762c40
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2022-02-07 19:08:31 +0300

    Change cli.py

---
+++ brain_games/cli.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import prompt
+
+def welcome_user():
+    print('Welcome to the BRain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, 'name + '!')` 
+            
+
+if __name__ == '__main__':
+    welcome_user()
