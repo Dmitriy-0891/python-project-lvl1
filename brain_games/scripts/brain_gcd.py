99e61c8b0e2d38b9890250e4d9dbd9db9d68a789
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2022-02-16 23:13:03 +0300

    Add brain_gcd.py, add script brain-gcd in pyprject.toml

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,37 @@
+#!:/usr/bin/env python3
+
+import prompt
+import random
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, ' + name + '!')
+    print('Find the greatest common divisor of given numbers.')
+    x = 1
+    while x <= 4:
+        if x == 4:
+            print("Congratulations, " + name + "!")
+        else:
+            first_count = random.randint(1,1000)
+            second_count = random.randint(1, 1000)
+            print("Question: " + str(first_count) + " " + str(second_count))
+            answer = prompt.string('Your answer: ')
+            if first_count > second_count:
+                #possible greatest common divisor
+                pgcd = second_count
+            else:
+                pgcd = first_count
+            for i in range(1, pgcd, 1):
+                if((first_count / i == 0) and (second_count / i == 0)):
+                    #greatest common divisor
+                    gcd = i
+                    if answer == str(gcd):
+                        print("Correct!")
+                    else:
+                        print( "'" + answer + "' is wrong answer:(. Correct answer is " + str(gcd) + ".\nLet`s try again " + name + "!")
+                        break
+        x += 1
+
+if __name__ == '__main__':
+    main()
