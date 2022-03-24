4d10e1db193e0f47dfb419e046b3feb18012ebee
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2022-03-24 23:22:08 +0300

    Changed brain_prime.py according to flake8

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,9 +3,10 @@
 import prompt
 import random
 
+
 def main():
     x = 0
-    while x >= 0: 
+    while x >= 0:
         print('Welcome to the Brain Games!')
         name = prompt.string('May I have your name? ')
         print('Hello, ' + name + '!')
@@ -13,20 +14,24 @@ def main():
         num = random.randint(0, 3752)
         print('Question: ' + str(num))
         answer = prompt.string('Your answer: ')
-        d = 2 
+        d = 2
         while num % d != 0:
             d += 1
         if num == d:
             if answer == "yes":
                 print("Correct!")
             else:
-                print("'" + answer + "' is wrong answer:(. Correct answer is 'yes'.\nLet`s try again " + name + "!")
+                print("'" + answer + "' is wrong answer:(. Correct answer is 'yes'.")
+                print("Let`s try again " + name + "!")
         else:
             if answer == "no":
                 print("Correct!")
             else:
-                print("'" + answer + "' is wrong answer:(. Correct answer is 'no'.\nLet`s try again " + name + "!")
+                print("'" + answer + "' is wrong answer:(. Correct answer is 'no'.")
+                print("Let`s try again " + name + "!")
+        
         x += 1
+
 
 if __name__ == '__main__':
     main()
