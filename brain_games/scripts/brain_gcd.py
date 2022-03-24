cc91b8d7c1e0d47e71763565b21ff750714b5fd5
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2022-03-24 23:53:17 +0300

    Changed brain_gcd.py according to flake8

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,6 +3,7 @@
 import prompt
 import random
 
+
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
@@ -13,27 +14,30 @@ def main():
         if x == 4:
             print("Congratulations, " + name + "!")
         else:
-            first_count = random.randint(1,1000)
-            second_count = random.randint(1, 1000)
-            print("Question: " + str(first_count) + " " + str(second_count))
-            answer = prompt.string('Your answer: ')
+            num_1 = random.randint(1, 1000)
+            num_2 = random.randint(1, 1000)
+            print("Question: " + str(num_1) + " " + str(num_2))
+            a = prompt.string('Your answer: ')
             pgcd = 1
-            if first_count > second_count:
-                #possible greatest common divisor
-                pgcd = second_count
+            if num_1 > num_2:
+                # possible greatest common divisor
+                pgcd = num_2
             else:
-                pgcd = first_count
+                pgcd = num_1
             gcd = 1
             for i in range(pgcd, 1, -1):
-                if((first_count % i == 0) and (second_count % i == 0)):
-                    #greatest common divisor
+                if((num_1 % i == 0) and (num_2 % i == 0)):
+                    # greatest common divisor
                     gcd = i
-            if answer == str(gcd):
+            if a == str(gcd):
                 print("Correct!")
             else:
-                print( "'" + answer + "' is wrong answer:(. Correct answer is " + str(gcd) + ".\nLet`s try again " + name + "!")
+                print("'" + a + "' is wrong answer:(. Correct answer is " + str(gcd) + ".")
+                print("Let`s try again " + name + "!")
                 break
+        
         x += 1
+
 
 if __name__ == '__main__':
     main()
