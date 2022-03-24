ba07cf8967c7c771a5879bbea4bc37fdac1467c1
Dimitrii Chimidov <cimidovdmitrii@gmail.com>
2022-03-24 23:41:25 +0300

    Changed brain_calc.py according to flake8

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,6 +3,7 @@
 import prompt
 import random
 
+
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
@@ -13,34 +14,40 @@ def main():
         if x == 4:
             print("Congratulations, " + name + "!")
         else:
-            first_count = random.randint(1,99)
-            second_count = random.randint(1, 99)
+            num_1 = random.randint(1, 99)
+            num_2 = random.randint(1, 99)
             list_arithmetic = ['+', '-', '*']
-            arifm_operator = random.choice(list_arithmetic)
-            print("Question: " + str(first_count) + " " + str(arifm_operator) + " " + str(second_count))
+            sign = random.choice(list_arithmetic)
+            print("Question: " + str(num_1) + " " + str(sign) + " " + str(num_2))
             answer = prompt.string('Your answer: ')
-            if arifm_operator == "+":
-                result = first_count + second_count
+            if sign == "+":
+                result = num_1 + num_2
                 if answer == str(result):
                     print("Correct!")
                 else:
-                    print( "'" + answer + "' is wrong answer:(. Correct answer is " + str(result) + ".\nLet`s try again " + name + "!")
+                    print("'" + answer + "' is wrong answer:(. Correct answer is " + str(result) + ".")
+                    print("Let`s try again " + name + "!")
                     break
-            elif arifm_operator == "-":
-                result = first_count - second_count
+            elif sign == "-":
+                result = num_1 - num_2
                 if answer == str(result):
                     print("Correct!")
                 else:
-                    print( "'" + answer + "' is wrong answer:(. Correct answer is " + str(result) + ".\nLet`s try again " + name + "!")
+                    print("'" + answer + "' is wrong answer:(. Correct answer is " + str(result) + ".")
+                    print("Let`s try again " + name + "!")
                     break
-            elif arifm_operator == "*":
-                result = first_count * second_count
+            elif sign == "*":
+                result = num_1 * num_2
                 if answer == str(result):
                     print("Correct!")
                 else:
-                    print( "'" + answer + "' is wrong answer:(. Correct answer is " + str(result) + ".\nLet`s try again " + name + "!")
+                    print("'" + answer + "' is wrong answer:(. Correct answer is " + str(result) + ".")
+                    print("Let`s try again " + name + "!")
                     break
+
+
         x += 1
+
 
 if __name__ == '__main__':
     main()
